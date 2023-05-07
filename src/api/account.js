0d036dd409d96666416cee76810716940f860422
sky6862 <756450010@qq.com>
2023-05-07 23:25:21 +0800

    获取账号列表api-getAccountList

--- src/api/account.js
+++ src/api/account.js
@@ -1,3 +1,8 @@
 import request from '../utils/request'
 
+// 登录
 export const login = (data)=>request.post("/users/checkLogin",data)
+// 账号列表
+export const getAccountList =  (data)=>{
+   return request.get("/users/list",data);
+}
